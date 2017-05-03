9dbd3044623c8a5a3737dd5aff087e3e3af90bfc
lonikolord <lino.coltura@hotmail.com>
2017-05-03 14:57:07 +0200

    database update

--- database/bemybutler.sql
+++ database/bemybutler.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2017 at 04:49 PM
+-- Generation Time: May 03, 2017 at 02:56 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -23,6 +23,56 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `drinks`
+--
+
+CREATE TABLE `drinks` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `drinks`
+--
+
+INSERT INTO `drinks` (`id`, `name`, `price`) VALUES
+(1, 'Maes', 2),
+(2, 'Cola', 2),
+(3, 'Fanta', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `serviceID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `open` tinyint(1) NOT NULL DEFAULT '1',
+  `amount` int(2) NOT NULL,
+  `completed` tinyint(1) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `useriscustomer`
+--
+
+CREATE TABLE `useriscustomer` (
+  `id` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `serviceID` int(11) NOT NULL,
+  `drink` varchar(100) NOT NULL,
+  `complete` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -38,19 +88,42 @@ CREATE TABLE `users` (
   `picture` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `link` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
-  `modified` datetime NOT NULL
+  `modified` datetime NOT NULL,
+  `balance` int(3) NOT NULL DEFAULT '0',
+  `loyalty` int(5) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `oauth_provider`, `oauth_uid`, `first_name`, `last_name`, `email`, `gender`, `locale`, `picture`, `link`, `created`, `modified`) VALUES
-(1, 'facebook', '1444507322266434', 'Lino', 'Coltura', 'lino.coltura@hotmail.com', 'male', 'en_US', 'https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/13912590_1189662361084266_4920162799492372226_n.jpg?oh=3603d4f6abdc807222c162bbce206bfb&oe=5996CB0C', 'https://www.facebook.com/app_scoped_user_id/1444507322266434/', '2017-04-19 15:17:20', '2017-04-19 16:46:28');
+INSERT INTO `users` (`id`, `oauth_provider`, `oauth_uid`, `first_name`, `last_name`, `email`, `gender`, `locale`, `picture`, `link`, `created`, `modified`, `balance`, `loyalty`) VALUES
+(1, 'facebook', '1444507322266434', 'Lino', 'Coltura', 'lino.coltura@hotmail.com', 'male', 'en_US', 'https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/13912590_1189662361084266_4920162799492372226_n.jpg?oh=5081480f12b79fc42cc6307ad97527d2&oe=59BE580C', 'https://www.facebook.com/app_scoped_user_id/1444507322266434/', '2017-04-19 15:17:20', '2017-05-03 14:53:21', 20, 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `drinks`
+--
+ALTER TABLE `drinks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`serviceID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `useriscustomer`
+--
+ALTER TABLE `useriscustomer`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `serviceID` (`serviceID`);
 
 --
 -- Indexes for table `users`
@@ -63,10 +136,36 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `drinks`
+--
+ALTER TABLE `drinks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `serviceID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `useriscustomer`
+--
+ALTER TABLE `useriscustomer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `useriscustomer`
+--
+ALTER TABLE `useriscustomer`
+  ADD CONSTRAINT `useriscustomer_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `useriscustomer_ibfk_3` FOREIGN KEY (`serviceID`) REFERENCES `services` (`serviceID`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
