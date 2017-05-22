93118d60d22ac5b58d33daf168b64ff7a59f419d
TheMoya <Moya@MacBook-Pro-van-Moya.local>
2017-05-22 21:02:35 +0200

    Center Fix Facebook Button

--- login.php
+++ login.php
@@ -100,21 +100,7 @@ if(isset($accessToken)){
 <!doctype HTML>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>LOGIN</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="css/reset.css">
-    <link rel="stylesheet" type="text/css" href="css/normalize.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/screen.css">
-
-    <!-- JavaScript -->
-    <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
-    <script type="text/javascript" src="js/script.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-
+  <?php include 'includes/header.php' ?>
 </head>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
